f11d04d1f9965c4bbd68e3b6c3f3452fbdd0533f
Jen Looper <jen.looper@gmail.com>
2020-08-21 16:27:12 -0400

    all working except change of icon color

--- src/index.js
+++ src/index.js
@@ -1,50 +1,94 @@
 import axios from 'axios';
+
+// form fields
+const form = document.querySelector('.form-data');
+const region = document.querySelector('.region-name');
+const apiKey = document.querySelector('.api-key');
+
+// results
 const errors = document.querySelector('.errors');
 const loading = document.querySelector('.loading');
+const results = document.querySelector('.result-container');
 const usage = document.querySelector('.carbon-usage');
 const fossilfuel = document.querySelector('.fossil-fuel');
+const myregion = document.querySelector('.my-region');
 
-const results = document.querySelector('.result-container');
-results.style.display = 'none';
-loading.style.display = 'none';
-errors.textContent = '';
-// grab the form
-const form = document.querySelector('.form-data');
-// grab the region name
-const region = document.querySelector('.region-name');
+//if anything is in localStorage, pick it up
+const storedApiKey = localStorage.getItem('apiKey');
+const storedRegion = localStorage.getItem('regionName');
 
-// declare a method to search by country name
-const searchForRegion = async (regionName) => {
-	loading.style.display = 'block';
-	errors.textContent = '';
+const displayCarbonUsage = async (apiKey, region) => {
 	try {
 		const response = await axios.get('https://api.co2signal.com/v1/latest', {
 			params: {
-				countryCode: regionName,
+				countryCode: region,
 			},
 			headers: {
-				//use your own token please
-				'auth-token': 'c099158761a3a563',
+				//please get your own token from CO2Signal https://www.co2signal.com/
+				'auth-token': apiKey,
 			},
 		});
 		console.log(response);
+
+		let co2Scale = [0, 150, 600, 750, 800];
+		let colors = ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'];
+		let CO2 = Math.floor(response.data.data.carbonIntensity);
+
+		let closestNum = co2Scale.sort((a, b) => {
+			return Math.abs(a - CO2) - Math.abs(b - CO2);
+		})[0];
+
+		let scaleIndex = co2Scale.findIndex((co2Scale) => co2Scale === closestNum);
+
+		let closestColor = colors[scaleIndex];
+
+		chrome.runtime.sendMessage({ action: 'updateIcon', value: { number: closestNum, color: closestColor } });
+
 		loading.style.display = 'none';
-		usage.textContent = response.data.data.carbonIntensity;
-		fossilfuel.textContent = response.data.data.fossilFuelPercentage;
+		form.style.display = 'none';
+		myregion.textContent = region;
+		usage.textContent =
+			Math.round(response.data.data.carbonIntensity) + ' grams (grams C02 emitted per kilowatt hour)';
+		fossilfuel.textContent =
+			response.data.data.fossilFuelPercentage.toFixed(2) +
+			'% (percentage of fossil fuels used to generate electricity)';
 		results.style.display = 'block';
 	} catch (error) {
 		console.log(error);
 		loading.style.display = 'none';
 		results.style.display = 'none';
-		errors.textContent = 'We have no data for the region you have requested.';
+		errors.textContent = 'Sorry, we have no data for the region you have requested.';
 	}
+};
+
+// set up api key and region
+const setUpUser = async (apiKey, regionName) => {
+	localStorage.setItem('apiKey', apiKey);
+	localStorage.setItem('regionName', regionName);
+	loading.style.display = 'block';
+	errors.textContent = '';
+	//make initial call
+	displayCarbonUsage(apiKey, regionName);
 };
 
 // declare a function to handle form submission
 const handleSubmit = async (e) => {
 	e.preventDefault();
-	searchForRegion(region.value);
-	console.log(region.value);
+	setUpUser(apiKey.value, region.value);
 };
+
+//initial check to see if repeat visitor
+if (storedApiKey === null && storedRegion === null) {
+	//if we don't have the keys, show the form
+	form.style.display = 'visible';
+	results.style.display = 'none';
+	loading.style.display = 'none';
+	errors.textContent = '';
+} else {
+	//if we have saved keys/regions in localStorage, show results when they load
+	results.style.display = 'none';
+	form.style.display = 'none';
+	displayCarbonUsage(storedApiKey, storedRegion);
+}
 
 form.addEventListener('submit', (e) => handleSubmit(e));
