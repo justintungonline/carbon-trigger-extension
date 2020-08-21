c791e7c36a5311d272fb80f919eccfd7c796ce6f
Jen Looper <jen.looper@gmail.com>
2020-08-20 21:00:28 -0400

    a carbon trigger chrome/edge extension!

--- README.md
+++ README.md
@@ -1,51 +1,37 @@
-# Covtension
+# Carbon Trigger Browser Extension
 
-A simple chrome extension built in HTML, CSS and JS that allows users search for live coronavirus data on any country.
+Huge props to Adebola who build a useful [extension](https://github.com/onedebos/covtension) for Chrome and Edge to track COVID country by country via API calls. He wrote an excellent [article](https://blog.adebola.dev/how-to-build-a-chrome-extension-that-makes-api-calls/) about it.
 
-## Video demo
-
-![](./covtension.gif)
+I have taken this code and edited it to work with tmrow's API to track electricity usage, so that you can have a reminder right in your browser about how heavy your region's electricity usage is, and to make judgement calls on your activities based on this information.
 
 ## Getting Started
 
-**To get started, follow the instructions below**
+To get a local copy of this extension up and running follow these steps.
 
-To get a local copy up and running follow these simple steps.
-
-- git clone the repo
+-   git clone the repo
 
 ```
-git clone https://github.com/onedebos/covtension && cd covtension
+git clone https://github.com/jlooper/carbontrigger && cd carbontrigger
 ```
 
-- Install all the required packages with
+-   Install all the required packages with
 
 ```
 npm install
 ```
 
-- Build from webpack
+-   Build from webpack
 
 ```
 npm run build
 ```
 
-To install in Chrome
-
-- Head over to chrome://extensions
-- Toggle "Developer mode" on.
-- Click Load unpacked.
-- Upload the dist folder to chrome
-- The extension should now be available for use
-
-### Automated Tests
-
-- Currently, there are no automated tests avaailable for this project. This will be implemented at a later date.
+To install on Edge
 
 ## Authors
 
-👤 **Adebola**
+Original author is Adebola Adeniran with his impressive 'covtension' Coronavirus extension. Follow him here:
 
-- Github: [@githubhandle](https://github.com/onedebos)
-- Twitter: [@twitterhandle](https://twitter.com/debosthefirst)
-- Linkedin: [linkedin](https://www.linkedin.com/in/adebola-niran/)
+-   Github: [@onedebos](https://github.com/onedebos)
+-   Twitter: [@debosthefirst](https://twitter.com/debosthefirst)
+-   Linkedin: [adebola-niran](https://www.linkedin.com/in/adebola-niran/)
--- src/index.js
+++ src/index.js
@@ -1,42 +1,50 @@
-import axios from "axios";
-const api = "https://covid19.mathdro.id/api/countries";
-const errors = document.querySelector(".errors");
-const loading = document.querySelector(".loading");
-const cases = document.querySelector(".cases");
-const recovered = document.querySelector(".recovered");
-const deaths = document.querySelector(".deaths");
-const results = document.querySelector(".result-container");
-results.style.display = "none";
-loading.style.display = "none";
-errors.textContent = "";
+import axios from 'axios';
+const errors = document.querySelector('.errors');
+const loading = document.querySelector('.loading');
+const usage = document.querySelector('.carbon-usage');
+const fossilfuel = document.querySelector('.fossil-fuel');
+
+const results = document.querySelector('.result-container');
+results.style.display = 'none';
+loading.style.display = 'none';
+errors.textContent = '';
 // grab the form
-const form = document.querySelector(".form-data");
-// grab the country name
-const country = document.querySelector(".country-name");
+const form = document.querySelector('.form-data');
+// grab the region name
+const region = document.querySelector('.region-name');
 
 // declare a method to search by country name
-const searchForCountry = async countryName => {
-  loading.style.display = "block";
-  errors.textContent = "";
-  try {
-    const response = await axios.get(`${api}/${countryName}`);
-    loading.style.display = "none";
-    cases.textContent = response.data.confirmed.value;
-    recovered.textContent = response.data.recovered.value;
-    deaths.textContent = response.data.deaths.value;
-    results.style.display = "block";
-  } catch (error) {
-    loading.style.display = "none";
-    results.style.display = "none";
-    errors.textContent = "We have no data for the country you have requested.";
-  }
+const searchForRegion = async (regionName) => {
+	loading.style.display = 'block';
+	errors.textContent = '';
+	try {
+		const response = await axios.get('https://api.co2signal.com/v1/latest', {
+			params: {
+				countryCode: regionName,
+			},
+			headers: {
+				//use your own token please
+				'auth-token': 'c099158761a3a563',
+			},
+		});
+		console.log(response);
+		loading.style.display = 'none';
+		usage.textContent = response.data.data.carbonIntensity;
+		fossilfuel.textContent = response.data.data.fossilFuelPercentage;
+		results.style.display = 'block';
+	} catch (error) {
+		console.log(error);
+		loading.style.display = 'none';
+		results.style.display = 'none';
+		errors.textContent = 'We have no data for the region you have requested.';
+	}
 };
 
 // declare a function to handle form submission
-const handleSubmit = async e => {
-  e.preventDefault();
-  searchForCountry(country.value);
-  console.log(country.value);
+const handleSubmit = async (e) => {
+	e.preventDefault();
+	searchForRegion(region.value);
+	console.log(region.value);
 };
 
-form.addEventListener("submit", e => handleSubmit(e));
+form.addEventListener('submit', (e) => handleSubmit(e));
